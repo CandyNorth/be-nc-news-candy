901ec4c0d8d49dcd7b1673509769421b5a937ada
Candy Davies <candydaviesnorth@gmail.com>
2024-08-29 23:10:44 +0100

    updates connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,12 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 const config = {};
 
-
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+  throw new Error("PGDATABASE and DATABASE_URL not set");
 }
 
 if (ENV === "production") {
