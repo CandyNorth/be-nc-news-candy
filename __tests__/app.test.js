d1e09f32db29a912392afcda9cc4be6ae31124e1
Candy Davies <candydaviesnorth@gmail.com>
2024-08-29 02:33:39 +0100

    update /api/topics tests, app.js topics controller, model and server

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const app = require("../app");
+const { app } = require("../app");
 const request = require("supertest");
 const req = request(app);
 const db = require("../db/connection");
@@ -13,38 +13,54 @@ const {
 beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 afterAll(() => db.end());
 
-describe("/api/topics", () => {
-  test("GET:200 sends an array of topics to the client", () => {
-    return req
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(Array.isArray(body.topics)).toBe(true);
-        expect(body.topics).toHaveLength(3);
-      });
-  });
-  test("GET:200 each topic has the correct properties", () => {
-    return req
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        body.topics.forEach((topic) => {
-          expect(topic).toHaveProperty("slug");
-          expect(topic).toHaveProperty("description");
-          expect(typeof topic.slug).toBe("string");
-          expect(typeof topic.description).toBe("string");
+describe("app.js", () => {
+  describe("/api/topics", () => {
+    test("GET:200 sends an array of topics to the client", () => {
+      return req
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body.topics)).toBe(true);
+          expect(body.topics).toHaveLength(3);
         });
-      });
-  });
-  test("GET:404 responds with an error for a non-existent path", () => {
-    return req
-      .get("/api/topcsx")
-      .expect(404)
-      .then((response) => {
-        //console.log(response);
-        // console.log(response.request.res.statusMessage);
-        expect(response.request.res.statusMessage).toBe("Not Found");
-        expect(response.status).toBe(404);
-      });
+    });
+    test("GET:200 each topic has the correct properties", () => {
+      return req
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          body.topics.forEach((topic) => {
+            expect(topic).toHaveProperty("slug");
+            expect(topic).toHaveProperty("description");
+            expect(typeof topic.slug).toBe("string");
+            expect(typeof topic.description).toBe("string");
+          });
+        });
+    });
+    test("GET:404 comes back with an error for a path that doesnt exist", () => {
+      return req
+        .get("/api/topcsx")
+        .expect(404)
+        .then((response) => {
+          expect(response.request.res.statusMessage).toBe("Not Found");
+          expect(response.status).toBe(404);
+        });
+    });
+    test("POST:405 will return a  'Method Not Allowed' for unsupported HTTP method", () => {
+      return req
+        .post("/api/topics")
+        .expect(405)
+        .then(({ body }) => {
+          expect(body.msg).toBe("Method Not Allowed");
+        });
+    });
+    test("DELETE:405 will return 'Method Not Allowed' for an unsupported HTTP method", () => {
+      return req
+        .delete("/api/topics")
+        .expect(405)
+        .then(({ body }) => {
+          expect(body.msg).toBe("Method Not Allowed");
+        });
+    });
   });
 });
--- models/topics-model.js
+++ models/topics-model.js
@@ -1,16 +1,8 @@
 const db = require("../db/connection");
-const format = require('pg-format');
+const format = require("pg-format");
 
 exports.selectTopics = () => {
-  // db.query(format('SELECT * FROM %I', "topics")).then((result) => console.log(result.rows))
-  return db.query(format('SELECT * FROM %I', "topics"))
+  return db
+    .query(format("SELECT * FROM %I", "topics"))
     .then((result) => result.rows);
 };
-// exports.selectTopics = (topic) => {
-//     return readFile(`data/topic/${catId}.json`, "utf8").then((catString, err) => {
-//         const parsedCat = JSON.parse(catString);
-//         return parsedCat;
-//       }).catch((err) => {
-//         return err;
-//       });
-//     };
